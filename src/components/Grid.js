1b01b67755bf4d1eda65c6b3c39847ad69eeaa66
Kyrylo Glamazdin <kyrylo.glamazdin36@myhunter.cuny.edu>
2020-06-16 16:48:14 -0400

    added comments to the functions and some css

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,11 +1,12 @@
-import React, {Component} from 'react';
+import React from 'react';
 import Row from './Row.js';
+import '../styles/Grid.css';
 
+//the Grid component represents the entire grid with all of the rows and columns
 const Grid = ({ rows, color, setCellColor, setActive, activeColoring, activeDrawing, setInactive }) => {
     
     return(
-        <main>
-        <h1>So I'm thinking the color of each cell must be the actual value stored in the corresponding cell in the array. Then each Col element would set its backgroundColor propertly by using the value stored in that place in 'rows' array</h1>
+        <main className="main-grid">
             {rows.map((col, index) => (<Row key={index} cols={col} color={color} indexr={index} setCellColor={setCellColor} setActive={setActive} activeColoring={activeColoring} activeDrawing={activeDrawing}setInactive = {setInactive}/>))}
         </main>
     )
