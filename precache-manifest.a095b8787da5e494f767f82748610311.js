4a0b4e3e8713efd585e34a5c30b1261499529c41
Kyrylo Glamazdin <kyrylo.glamazdin36@myhunter.cuny.edu>
2020-06-18 11:46:15 -0400

    Updates

---
+++ precache-manifest.a095b8787da5e494f767f82748610311.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6c9de10a5ffbf3657878adb457e5026",
+    "url": "/React-Grid/index.html"
+  },
+  {
+    "revision": "4b298766c6813af57fcf",
+    "url": "/React-Grid/static/css/main.bf47d6c0.chunk.css"
+  },
+  {
+    "revision": "681dfd468e95fad08d76",
+    "url": "/React-Grid/static/js/2.4d8057e4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Grid/static/js/2.4d8057e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b298766c6813af57fcf",
+    "url": "/React-Grid/static/js/main.8b6256d6.chunk.js"
+  },
+  {
+    "revision": "0a07490271769b0a7192",
+    "url": "/React-Grid/static/js/runtime-main.c34d534a.js"
+  }
+]);
